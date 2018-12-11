c32bf9a4db98986a82868ac599f546d296a5c7b5
camron <camronmoradi@gmail.com>
2018-12-11 16:01:52 +0100

    Improves performance_data specs to ensure return data is just for the given user

--- spec/requests/api/v1/performance_data_spec.rb
+++ spec/requests/api/v1/performance_data_spec.rb
@@ -1,8 +1,14 @@
 RSpec.describe Api::V1::PerformanceDataController, type: :request do
-        let(:user) { FactoryBot.create(:user) }
-        let(:credentials) { user.create_new_auth_token }
-        let(:headers) { { HTTP_ACCEPT: 'application/json' }.merge!(credentials) }
-    
+        let(:user1) { FactoryBot.create(:user, email: 'user1@random.com') }
+        let(:user2) { FactoryBot.create(:user, email: 'user2@random.com') }
+
+        let(:credentials) { user1.create_new_auth_token } 
+        # token for user1 that the backend creates
+
+        let(:headers) { { HTTP_ACCEPT: 'application/json' }.merge!(credentials) } 
+        # basically says the data I'm sending will be in json format
+        # 'headers' is info about the type of data you're sending and some technical data related to that response request
+        
     describe 'POST /api/v1/performance_data' do
         it 'creates a data entry' do
             post '/api/v1/performance_data', params: {
@@ -16,14 +22,16 @@ RSpec.describe Api::V1::PerformanceDataController, type: :request do
 
     describe 'GET /api/v1/performance_data' do
         before do
-            5.times { user.performance_data.create(data: { message: 'Average' }) }
+            5.times { user1.performance_data.create(data: { message: 'Average' }) }
+            5.times { user2.performance_data.create(data: { message: 'Average' }) }
         end
       
         it 'returns a collection of performance data for the given user' do
             get '/api/v1/performance_data', headers: headers
+            # binding.pry
             expect(response_json['entries'].count).to eq 5
             response_json['entries'].each do |entry|
-                expect(entry['user_id']).to eq user.id
+                expect(entry['user_id']).to eq user1.id
             end
         end  
     end
